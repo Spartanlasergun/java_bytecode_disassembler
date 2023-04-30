eadda207ef28e049cf35c8ae44ec9c14cb8b6900
Spartanlasergun <bns360@live.com>
2023-04-30 17:26:53 -0400

    new test files

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,8 @@
 import sys
 sys.path.append('C:/Users/bns36/Documents/java_bytecode_disassembler/src/disassembler')
 
-from java_bytecode_disassembler import java_bytecode_disassembler
+from java_bytecode_disassembler import disassembler
 
-java_bytecode_disassembler('Main.class', verbose=True)
+disassembly = disassembler('Main.class', verbose=True)
+
+
